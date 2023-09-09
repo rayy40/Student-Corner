dedb2a5b96471359c6e4e0c4d230d9986b60bc17
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-10 02:00:54 +0530

    Added convex

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const convex_config = {
+    providers: [
+      {
+        domain: "https://funny-mudfish-70.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default convex_config
