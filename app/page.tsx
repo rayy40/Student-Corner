6d9a25cb808a9d855d6ccb8dd1b4f9c3ab478fac
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-10 14:28:36 +0530

    Refactored styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import {FaArrowRight} from "react-icons/fa6"
+import useStoreUserEffect from "@/hooks/useStoreUserEffect";
 import Link from "next/link"
 
 export default function Home() {
+  const userId = useStoreUserEffect();
 
   type SectionProps = {
     link: string,
@@ -15,7 +19,7 @@ export default function Home() {
   }:SectionProps) => {
     return (
       <div className="flex flex-col gap-2 border-b last:border-b-0 py-10 border-y-light-gray">
-      <div className="flex flex-row justify-space-between items-center">
+      <div className="flex flex-row justify-between items-center">
         <div className="flex flex-col flex-[0.6] gap-4">
           <h3 className="text-lg font-medium">{title}</h3>
           <p>{description}</p>
@@ -34,7 +38,7 @@ export default function Home() {
   }
 
   return (
-    <main className="p-6">
+    <main className="mt-6 p-6">
       <div className="mx-auto w-4/5 max-w-screen-lg">
         <Sections link="/quizify" title="Quizify" description="Create personalized quizzes from PDFs and text on any topic with our dynamic quiz generator." />
         <Sections link="/ai-pen" title="AI-Pen" description=" Instantly create and edit articles, essays, and blogs with powerful AI assistance." />
