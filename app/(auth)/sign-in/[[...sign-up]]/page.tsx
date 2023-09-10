996e373cb7bad5c0c8dd053832fdcb47051c902a
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-10 14:27:53 +0530

    Custom authentication

--- app/(auth)/sign-in/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-up]]/page.tsx
@@ -1,15 +1,62 @@
 "use client"
 
-import { SignIn } from "@clerk/clerk-react"
+import {useState} from "react";
+import { useSignIn } from "@clerk/clerk-react"
+import {useRouter} from "next/navigation";
 import Link from "next/link"
 
 export default function SignInPage() {
+  const {isLoaded, signIn,setActive} = useSignIn();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if(!isLoaded) {
+      return;
+    }
+
+    try {
+      const result = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      if (result.status === "complete") {
+        console.log(result);
+        await setActive({ session: result.createdSessionId });
+        router.push("/")
+      }
+      else {
+        /*Investigate why the login hasn't completed */
+        console.log(result);
+      }
+
+    } catch (error) {
+      console.error(JSON.stringify(error, null, 2))
+    }
+  }
+
   return (
     <div className="w-100 h-screen flex flex-col justify-center items-center bg-white">
-        <SignIn />
-        <div className="flex items-center justify-center">
-            <p className="text-xs text-light-gray">No acccount? <Link href="/sign-up" className="text-slate cursor-pointer">Sign Up</Link></p>
+      <div className="w-full max-w-[360px] p-3 flex flex-col gap-5 items-center">
+        <h2 className="text-2xl font-medium">Sign In</h2>       <form className="w-full flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+          <input className="text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email address" />
+          </div>
+          <div className="flex flex-col gap-1">
+          <input className="text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+          </div>
+          <button className="text-sm w-full p-2 h-[36px] flex items-center justify-center bg-sign-in-background text-sign-in shadow-[inset_0_0_0_1px_rgba(235,87,87,0.3)] rounded-md" type="submit" onClick={handleSubmit}>
+            Sign In
+          </button>
+        </form>
+        <div className="flex mt-2 items-center justify-center">
+            <p className="text-xs text-dark-gray">No account? <Link href="/sign-up" className="text-slate cursor-pointer">Sign Up</Link></p>
         </div>
+      </div>
     </div>
   )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,15 +1,97 @@
 "use client"
 
-import { SignUp } from "@clerk/clerk-react"
+import {useState} from "react";
+import { useSignUp } from "@clerk/clerk-react"
+import {useRouter} from "next/navigation";
 import Link from "next/link"
 
 export default function SignUpPage() {
+  const {isLoaded, signUp,setActive} = useSignUp();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPendingVerification, setIsPendingVerification] = useState(false);
+  const [code, setCode] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if(!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        firstName,
+        lastName,
+        emailAddress: email,
+        password
+      })
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code"
+      })
+
+      setIsPendingVerification(true)
+    } catch (error) {
+      console.error(JSON.stringify(error, null, 2))
+    }
+  }
+
+  const onPressVerify = async (e: React.FormEvent<HTMLButtonElement>) => { 
+    e.preventDefault();
+    if(!isLoaded) {
+      return;
+    }
+    try {
+      const completeSignUp = await signUp?.attemptEmailAddressVerification({
+        code,
+      });
+      if(completeSignUp?.status !== 'complete') { 
+        console.log(JSON.stringify(completeSignUp, null, 2))
+      }
+      if(completeSignUp?.status === 'complete') { 
+        await setActive({session: completeSignUp.createdSessionId });
+        router.push("/")
+      }
+    } catch (error) {
+      console.error(JSON.stringify(error, null, 2))
+    }
+  }
+
   return (
     <div className="w-100 h-screen flex flex-col justify-center items-center bg-white">
-        <SignUp />
-        <div className="flex items-center justify-center">
-            <p className="text-xs text-light-gray">Already have an account? <Link href="/sign-in" className="text-slate cursor-pointer">Sign In</Link></p>
+      <div className="w-full max-w-[360px] p-3 flex flex-col gap-5 items-center">
+        <h2 className="text-2xl font-medium">Sign Up</h2>
+{!isPendingVerification &&        <form className="w-full flex flex-col gap-2">
+  <div className="flex flex-row gap-2">
+    <input className="w-1/2 text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="name" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="First name" />
+    <input className="w-1/2 text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="name" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Last name" />
+  </div>
+          <div className="flex flex-col gap-1">
+          <input className="text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email address" />
+          </div>
+          <div className="flex flex-col gap-1">
+          <input className="text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+          </div>
+          <button className="text-sm w-full p-2 h-[36px] flex items-center justify-center bg-sign-in-background text-sign-in shadow-[inset_0_0_0_1px_rgba(235,87,87,0.3)] rounded-md" type="submit" onClick={handleSubmit}>
+            Sign Up
+          </button>
+        </form>}
+        {isPendingVerification && (
+          <form className="w-full flex flex-col gap-2 items-center justify-center">
+            <input className="w-full text-sm p-2 h-[36px] font-normal bg-input-background shadow-[inset_0_0_0_1px_rgba(15,15,15,0.1)] rounded-md" value={code} placeholder="Enter your verification code" onChange={(e) => setCode(e.target.value)} />
+            <button className="text-sm w-full p-2 h-[36px] flex items-center justify-center bg-sign-in-background text-sign-in shadow-[inset_0_0_0_1px_rgba(235,87,87,0.3)] rounded-md" type="submit" onClick={onPressVerify}>
+            Verify Email
+          </button>
+          </form>
+        )}
+        <div className="flex mt-2 items-center justify-center">
+            <p className="text-xs text-dark-gray">Already have an account? <Link href="/sign-in" className="text-slate cursor-pointer">Sign In</Link></p>
         </div>
+      </div>
     </div>
   )
 }
