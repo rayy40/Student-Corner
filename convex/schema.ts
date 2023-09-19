a378a37bc4aaf90f2e9371d084496c0a6870c957
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-19 06:00:26 +0530

    Updated schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,6 +6,29 @@ export default defineSchema({
     name: v.string(),
     tokenIdentifier: v.string(),
   }).index("by_token", ["tokenIdentifier"]),
+  chatbot: defineTable({
+    storageId: v.string(),
+    userId: v.id("users"),
+    format: v.string(),
+  }),
+  aipen: defineTable({
+    userId: v.id("users"),
+    input: v.optional(v.string()),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    lastOpenedOn: v.optional(v.string()),
+  }),
+  sources: defineTable({
+    name: v.string(),
+    chunkIds: v.array(v.id("chunks")),
+    saved: v.boolean(),
+  }),
+  chunks: defineTable({
+    text: v.string(),
+    sourceId: v.id("sources"),
+    chunkIndex: v.number(),
+    lines: v.object({ from: v.number(), to: v.number() }),
+  }),
   quiz: defineTable({
     userId: v.id("users"),
     input: v.object({
@@ -23,6 +46,7 @@ export default defineSchema({
         options: v.optional(v.array(v.string())),
       })
     ),
+    score: v.optional(v.number()),
     options: v.optional(v.any()),
   }),
 });
