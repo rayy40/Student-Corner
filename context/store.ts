76373576e37cc50e2717c036cfe51557b2134c7e
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-19 05:59:46 +0530

    Refctored store

--- context/store.ts
+++ context/store.ts
@@ -1,31 +1,30 @@
 import { create } from "zustand";
 import { Id } from "@/convex/_generated/dataModel";
 
-type SelectedAnswersType = {
-  [key: string]: string;
-};
-
-interface QuizEntriesType {
-  _id: Id<"quiz">;
-  _creationTime: number;
-  userId: Id<"users">;
-  input: {
-    topic?: string | undefined;
-    paragraph?: string | undefined;
-    file?: any;
-    type: string;
-    questions: number;
-  };
-  response: string;
-}
-[];
-
 interface QuizState {
   quizId: Id<"quiz"> | null;
   setQuizId: (quizId: Id<"quiz">) => void;
 }
 
+interface NotepadState {
+  context: string;
+  setContext: (context: string) => void;
+  documentId: Id<"aipen"> | null;
+  setDocumentId: (documentId: Id<"aipen">) => void;
+  isContextLoading: boolean;
+  setIsContextLoading: (isContextLoading: boolean) => void;
+}
+
 export const useQuizStore = create<QuizState>()((set) => ({
   quizId: null,
   setQuizId: (quizId) => set({ quizId }),
+}));
+
+export const useNotepadStore = create<NotepadState>()((set) => ({
+  context: "",
+  setContext: (context) => set({ context }),
+  documentId: null,
+  setDocumentId: (documentId) => set({ documentId }),
+  isContextLoading: false,
+  setIsContextLoading: (isContextLoading) => set({ isContextLoading }),
 }));
