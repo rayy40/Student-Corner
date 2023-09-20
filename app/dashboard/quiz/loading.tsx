169c2f905b19fe3b906ce29340e865e28f51933f
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-20 13:50:49 +0530

    Added loading page

---
+++ app/dashboard/quiz/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from "@/components/Loading/Loading";
+import React from "react";
+
+export default function LoadingPage() {
+  return (
+    <div className="h-screen 2-full flex items-center justify-center">
+      <Loading />
+    </div>
+  );
+}
