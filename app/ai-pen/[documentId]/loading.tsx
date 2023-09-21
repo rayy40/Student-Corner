4c22eccad8f3c84441e4d4d67a04504f5f4f18bc
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-21 16:02:08 +0530

    loading state full screen

--- app/ai-pen/[documentId]/loading.tsx
+++ app/ai-pen/[documentId]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function LoadingPage() {
   return (
-    <div className="h-screen 2-full flex items-center justify-center">
+    <div className="h-screen w-screen 2-full flex items-center justify-center">
       <Loading />
     </div>
   );
