f598cde862a1a0b1d9a48ffa45c9e33d5dc8a7b5
Rayyan Alam <rayyanalam650@gmail.com>
2023-09-21 15:20:59 +0530

    fixed flickering while rendering page

--- app/ai-pen/page.tsx
+++ app/ai-pen/page.tsx
@@ -8,6 +8,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useEffect } from "react";
 import { useNotepadStore, useUserStore } from "@/context/store";
 import { useUser } from "@clerk/clerk-react";
+import Loading from "@/components/Loading/Loading";
 
 export default function AiPen() {
   const { setDocumentId } = useNotepadStore();
@@ -25,21 +26,33 @@ export default function AiPen() {
     }
   }, [user, userId, router]);
 
-  const pageEntries = useQuery(api.aipen.getDocumentsByUserId, {
-    userId: userId as Id<"users">,
-  });
+  const pageEntries = useQuery(
+    api.aipen.getDocumentsByUserId,
+    userId !== null ? { userId: userId as Id<"users"> } : "skip"
+  );
 
   useEffect(() => {
-    if (pageEntries?.length) {
-      const documentId = pageEntries?.[pageEntries?.length - 1]?._id;
+    // Ensure pageEntries has data and is not empty before updating state
+    if (pageEntries && pageEntries.length > 0) {
+      const documentId = pageEntries[pageEntries.length - 1]._id;
       setDocumentId(documentId);
       router.push(`/ai-pen/${documentId}`);
     }
-  }, [pageEntries, router, setDocumentId]);
+  }, [pageEntries, setDocumentId, router]);
 
-  return (
-    <div className="p-6 mt-14 max-w-[600px] mx-auto">
-      <AiInput />
-    </div>
-  );
+  if (!pageEntries) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (!pageEntries?.length) {
+    return (
+      <div className="p-6 mt-14 max-w-[600px] mx-auto">
+        <AiInput />
+      </div>
+    );
+  }
 }
